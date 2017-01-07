d022e28816aade6f17a850b4a9db73584b28b7db
eri <alex-eri@ya.ru>
2017-01-07 18:09:35 +0300

    plaintext list filtered by country

--- get_ips.py
+++ get_ips.py
@@ -2,30 +2,34 @@ import socket
 from tempfile import NamedTemporaryFile
 import shutil
 import csv
+import logging
 
 
 def get_ips(row):
     try:
         addrs = list(set([str(i[4][0]) for i in socket.getaddrinfo(row[0], row[1], socket.AF_INET)]))
-        print row[0] + ' has ips: ' + ','.join(addrs)
+        logging.info(row[0] + ' has ips: ' + ','.join(addrs))
         if len(row) == 3:
             row.append(addrs)
         elif len(row) == 4:
             row[3] = addrs
     except:
         if len(row) == 3:
-            row.append('')
+            row.append([])
     finally:
         return row
 
+if __name__ == "__main__":
+    
+    logging.basicConfig(level=logging.INFO)
 
-filename = 'acs_url.csv'
-tempfile = NamedTemporaryFile(delete=False)
+    filename = 'acs_url.csv'
+    tempfile = NamedTemporaryFile(delete=False)
 
-with open(filename, 'rb') as csvFile, tempfile:
-    reader = csv.reader(csvFile, delimiter=',', quotechar='"')
-    writer = csv.writer(tempfile, delimiter=',', quoting=csv.QUOTE_ALL)
-    for row in reader:
-        writer.writerow(get_ips(row))
+    with open(filename, 'rb') as csvFile, tempfile:
+        reader = csv.reader(csvFile, delimiter=',', quotechar='"')
+        writer = csv.writer(tempfile, delimiter=',', quoting=csv.QUOTE_ALL)
+        for row in reader:
+            writer.writerow(get_ips(row))
 
-shutil.move(tempfile.name, filename)
+    shutil.move(tempfile.name, filename)
---
+++ get_ips_plain.py
@@ -0,0 +1,30 @@
+import socket
+import csv
+import sys
+import argparse
+import os
+import get_ips
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process some contiries.')
+    parser.add_argument('--country', metavar='', type=str, nargs='+', help='contiries (default: all)')
+    parser.add_argument('--file', help='file to write (default: stdout)')
+
+    args = parser.parse_args()
+
+    if args.file:
+        out = open(args.file,'w')
+    else:
+        out = sys.stdout
+
+    filename = 'acs_url.csv'
+    with open(filename, 'rb') as csvFile:
+        reader = csv.reader(csvFile, delimiter=',', quotechar='"')
+        if args.country:
+            reader = filter( lambda x: x[2] in args.country, reader)
+	for row in reader:
+            ret = get_ips.get_ips(row)
+            out.writelines([ip + os.linesep for ip in ret[3]])
+    
+    if args.file:
+        out.close()
